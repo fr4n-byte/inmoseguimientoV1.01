e799f4581067d2f504505802e342b4fbdf0453b1
fr4n-byte <sol5520@gmail.com>
2026-04-26 20:36:18 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'inmo-v1';
+const ARCHIVOS = ['/'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ARCHIVOS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
